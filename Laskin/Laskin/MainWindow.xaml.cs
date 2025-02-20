7507cb3504ff207ba2adcec0ed13aac9ab958b12
Kohila Karunakaran <125026379+KohilaKarunakaran@users.noreply.github.com>
2025-02-20 12:38:42 +0200

    Laskin lisatty

---
+++ Laskin/Laskin/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laskin
+{
+
+    public partial class MainWindow : Window
+    {
+        private int firstNumber = 0;
+        private int secondNumber = 0;
+        private bool isSecondNumber = false;
+        private bool isDivision = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null)
+            {
+                int number = int.Parse(button.Content.ToString());
+                if (!isSecondNumber)
+                {
+                    firstNumber = number;
+                    Display.Text = firstNumber.ToString();
+                    isSecondNumber = true;
+                }
+                else
+                {
+                    secondNumber = number;
+                    Display.Text = secondNumber.ToString();
+                }
+            }
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSecondNumber) // Ensure two numbers are selected
+            {
+                if (isDivision)
+                {
+                    if (secondNumber != 0)
+                    {
+                        double result = (double)firstNumber / secondNumber;
+                        Display.Text = result.ToString("F2");
+                    }
+                    else
+                    {
+                        Display.Text = "Error";
+                    }
+                }
+                else
+                {
+                    int result = firstNumber + secondNumber;
+                    Display.Text = result.ToString();
+                }
+                isSecondNumber = false;
+                isDivision = false; // Reset after operation
+            }
+        }
+
+        private void Division_Checked(object sender, RoutedEventArgs e)
+        {
+            isDivision = true;
+        }
+        private void Division_Unchecked(object sender, RoutedEventArgs e)
+        {
+            isDivision = false;
+        }
+    }
+}
